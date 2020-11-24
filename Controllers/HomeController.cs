2ba7f45189cdb79a9a562f4bb6ec3c0ce344a861
FabGtte <fabiengilliotte@gmail.com>
2020-11-24 11:58:07 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPRoutage2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        [Route("Hello/Say")] // The endpoint here is reached by calling `/Say` route
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        [Route("Hello/Yell")] // The endpoint here is reached by calling `/Yell` route
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+        
+    }
+}
---
+++ Controllers/HomeConvController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPRoutage2._0.Controllers
+{
+    public class HomeConvController : ControllerBase
+    {
+       
+        public String SayHello()
+        {
+            return "Hello ! par convention";
+        }
+
+        public String YellHello()
+        {
+            return "HELLO ! par convention";
+        }
+        
+    }
+}
