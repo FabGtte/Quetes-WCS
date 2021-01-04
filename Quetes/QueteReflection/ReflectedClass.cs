c201f058e6d9039309c8ed238307b302724eae41
Fabien Gilliotte <41702805+FabGtte@users.noreply.github.com>
2021-01-04 11:22:34 +0100

    Update and rename bla to ReflectedClass.cs

---
+++ Quetes/QueteReflection/ReflectedClass.cs
@@ -0,0 +1,31 @@
+using System;
+
+class ReflectedClass
+
+{
+
+    private Int32 _id;
+
+    private String Name { get; set; }
+
+
+    public ReflectedClass()
+
+    {
+
+        _id = 0;
+
+        Name = String.Empty;
+
+    }
+
+
+    private void DoSomething()
+
+    {
+
+        Console.WriteLine("I am doing something ...");
+
+    }
+
+}
